14fb71f2fa6b56fa2aa3b3815945306f84c0bf54
jakub <jakubvendos@gmail.com>
2025-08-22 22:58:47 +0200

    edit extract

--- src/extract.py
+++ src/extract.py
@@ -1,5 +1,5 @@
 from numpy import long
-from config import DAILY_VARS, TIMEZONE, FORECAST_DAYS, RAW_DIR, today,formatted_date,url,all_results,result,howManyVars, Request_timeout
+from config import DAILY_VARS, TIMEZONE, FORECAST_DAYS, RAW_DIR, today, formatted_date, url, all_results, result, howManyVars, Request_timeout
 from regions import REGIONS
 from datetime import datetime
 import requests
@@ -13,7 +13,6 @@ for region, (lan, lon) in REGIONS.items():
     latitude = lan
     longitude = lon
     urlQuery = f"{url}&latitude={latitude}&longitude={longitude}&daily={result}&timezone={TIMEZONE}&forecast_days={FORECAST_DAYS}"
-    #print(f"URL dla {region_name}:{urlQuery}")
     response = requests.get(urlQuery, timeout=Request_timeout)
     question = response.json()
     question['region'] = region_name
@@ -25,24 +24,23 @@ for rec in all_results:
     reg = rec.get("region")
     lat = rec.get("latitude")
     lon = rec.get("longitude")
-    day = rec.get("daily",{})
-    times = day.get("time",[])
+    day = rec.get("daily", {})
+    times = day.get("time", [])
     for i, t in enumerate(times):
         row = {
-            "region" : reg,
-            "date" : t,
-            "latitude" : lat,
-            "longitude" : lon,
+            "region": reg,
+            "date": t,
+            "latitude": lat,
+            "longitude": lon,
         }
         for var in DAILY_VARS:
-            vals = day.get(var,[])
-            #print(f"{var}: {vals}")
-            row[var]=vals[i] if len(vals) else None
+            vals = day.get(var, [])
+            row[var] = vals[i] if len(vals) else None
         flat_rows.append(row)
 
 print(flat_rows)
 
 filename = f"data/raw/{formatted_date}.json"
 
-with open(filename,"w",encoding="utf-8") as f:
-    json.dump(flat_rows,f,ensure_ascii=False,indent=2)
+with open(filename, "w", encoding="utf-8") as f:
+    json.dump(flat_rows, f, ensure_ascii=False, indent=2)
